76617fd1bda7c7a734da7a76a8afebbd85699097
magdiel marquez <magdielikari@gmail.com>
2026-01-06 22:24:09 -0500

    fix for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // Tu dominio completo
+  site: 'https://ITDELA.github.io',
+  // El nombre de tu repositorio con barras al inicio y final
+  base: '/masterclass/',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  build: {
+    assets: 'assets', // Esto cambia '_astro' por 'assets'
+  },
 });
